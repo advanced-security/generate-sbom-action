d3164f7b3444b59a1c3dd1a9c68ecab4c36b1166
Colin Dembovsky <colindembovsky@github.com>
2025-05-27 13:15:20 +0000

    test: enhance Jest configuration and improve SBOM tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }]
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(octokit|@octokit)/)'
+  ],
   verbose: true
 }
